37718eacd3275ed219c2828666046b5f910691e4
Eddie Hernandez <mchh.004@gmail.com>
2019-12-29 20:12:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,9 +11,8 @@ def find_max_value(array)
   x = array[0]
   array.length.times { |index|
     if array[index] > x
-      x = array[index]
+      x = array[index] }
     end
-  }
 end
 
 def find_min_value(array)
